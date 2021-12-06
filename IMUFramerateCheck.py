5bad5c78a605de4ee3a82577adbdaac1e10abcdc
xuhui <zhaoxuhui1993@qq.com>
2021-12-06 22:45:42 +0800

    Add files via upload

--- IMUFramerateCheck.py
+++ IMUFramerateCheck.py
@@ -2,6 +2,7 @@
 from matplotlib import pyplot as plt
 import sys
 import numpy as np
+import time
 
 
 def readTimestampsEuRoC(file_path, time_unit):
@@ -26,12 +27,17 @@ def readTimestampsEuRoC(file_path, time_unit):
 
 
 if __name__ == '__main__':
-    time_file = sys.argv[1]
-    time_unit = sys.argv[2]
+    # 用于检查IMU数据流帧率是否稳定
+
+    time_file = sys.argv[1]  # 待检查IMU数据文件路径
+    time_unit = sys.argv[2]  # IMU观测时间戳单位
 
     print("Loading IMU data ...")
     timestamps = readTimestampsEuRoC(time_file, time_unit)
     print("Loaded IMU data!")
+
+    cur_time = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
+    fout = open("./imu_report_" + cur_time + ".txt", 'w')
 
     time_intervals = []
     for i in range(1, len(timestamps)):
@@ -42,13 +48,36 @@ if __name__ == '__main__':
     min_interval = min(time_intervals)
     var_interval = np.var(time_intervals)
     mean_fps = 1.0 / mean_interval
+    max_difference = max_interval - min_interval
+
+    index_max = time_intervals.index(max_interval)
+    index_min = time_intervals.index(min_interval)
+    timestamp_max = timestamps[index_max]
+    timestamp_min = timestamps[index_min]
+
+    str_line1 = "max interval:" + str(max_interval) + "(sec), index:" + str(index_max) + "/" + str(
+        len(timestamps)) + ", timestamp:" + str(timestamp_max) + "\n"
+    str_line2 = "min interval:" + str(min_interval) + "(sec), index:" + str(index_min) + "/" + str(
+        len(timestamps)) + ", timestamp:" + str(timestamp_min) + "\n"
+    str_line3 = "mean interval:" + str(mean_interval) + "\n"
+    str_line4 = "variance interval:" + str(var_interval) + "\n"
+    str_line5 = "max interval difference:" + str(max_difference) + "\n"
+    str_line6 = "mean fps:" + str(mean_fps) + "\n"
+
+    fout.write(str_line1)
+    fout.write(str_line2)
+    fout.write(str_line3)
+    fout.write(str_line4)
+    fout.write(str_line5)
+    fout.write(str_line6)
 
     print("\nStatistics:")
-    print("max interval:", max_interval, "(sec)")
-    print("min interval:", min_interval, "(sec)")
-    print("mean interval:", mean_interval, "(sec)")
-    print("variance interval:", var_interval)
-    print("mean fps:", mean_fps)
+    print(str_line1)
+    print(str_line2)
+    print(str_line3)
+    print(str_line4)
+    print(str_line5)
+    print(str_line6)
     print("\nPlotting ...")
 
     plt.bar(range(len(time_intervals)), time_intervals)
@@ -57,5 +86,7 @@ if __name__ == '__main__':
     plt.title("Mean interval:" + str(round(mean_interval, 9)) +
               "(sec)\nMean FPS:" + str(round(mean_fps, 1)) +
               " Variance:" + str(round(var_interval, 5)))
-    plt.axhline(mean_interval, color='orange')
+    plt.axhline(mean_interval, color='orange', label='Mean')
+    plt.legend()
+    plt.savefig("./imu_report_" + cur_time + ".png", dpi=600)
     plt.show()
--- VisionFramerateCheck.py
+++ VisionFramerateCheck.py
@@ -3,11 +3,17 @@ from HaveFun import common
 from matplotlib import pyplot as plt
 import sys
 import numpy as np
+import time
 
 if __name__ == '__main__':
-    search_dir = sys.argv[1]
-    file_type = sys.argv[2]
-    time_unit = sys.argv[3]
+    # 用于检查影像流帧率是否稳定
+
+    search_dir = sys.argv[1]  # 待检查影像所在文件夹
+    file_type = sys.argv[2]  # 影像文件类型
+    time_unit = sys.argv[3]  # 影像时间戳单位
+
+    cur_time = time.strftime("%Y_%m_%d_%H_%M_%S", time.localtime())
+    fout = open("./vision_report_" + cur_time + ".txt", 'w')
 
     print("Loading image data ...")
     paths, names, files = common.findFiles(search_dir, file_type)
@@ -39,13 +45,36 @@ if __name__ == '__main__':
     min_interval = min(time_intervals)
     var_interval = np.var(time_intervals)
     mean_fps = 1.0 / mean_interval
+    max_difference = max_interval - min_interval
+
+    index_max = time_intervals.index(max_interval)
+    index_min = time_intervals.index(min_interval)
+    timestamp_max = timestamps[index_max]
+    timestamp_min = timestamps[index_min]
+
+    str_line1 = "max interval:" + str(max_interval) + "(sec), index:" + str(index_max) + "/" + str(
+        len(timestamps)) + ", timestamp:" + str(timestamp_max) + "\n"
+    str_line2 = "min interval:" + str(min_interval) + "(sec), index:" + str(index_min) + "/" + str(
+        len(timestamps)) + ", timestamp:" + str(timestamp_min) + "\n"
+    str_line3 = "mean interval:" + str(mean_interval) + "\n"
+    str_line4 = "variance interval:" + str(var_interval) + "\n"
+    str_line5 = "max interval difference:" + str(max_difference) + "\n"
+    str_line6 = "mean fps:" + str(mean_fps) + "\n"
+
+    fout.write(str_line1)
+    fout.write(str_line2)
+    fout.write(str_line3)
+    fout.write(str_line4)
+    fout.write(str_line5)
+    fout.write(str_line6)
 
     print("\nStatistics:")
-    print("max interval:", max_interval, "(sec)")
-    print("min interval:", min_interval, "(sec)")
-    print("mean interval:", mean_interval, "(sec)")
-    print("variance interval:", var_interval)
-    print("mean fps:", mean_fps)
+    print(str_line1)
+    print(str_line2)
+    print(str_line3)
+    print(str_line4)
+    print(str_line5)
+    print(str_line6)
     print("\nPlotting ...")
 
     plt.bar(range(len(time_intervals)), time_intervals)
@@ -54,5 +83,7 @@ if __name__ == '__main__':
     plt.title("Mean interval:" + str(round(mean_interval, 9)) +
               "(sec)\nMean FPS:" + str(round(mean_fps, 1)) +
               " Variance:" + str(round(var_interval, 5)))
-    plt.axhline(mean_interval, color='orange')
+    plt.axhline(mean_interval, color='orange', label='Mean')
+    plt.legend()
+    plt.savefig("./vision_report_" + cur_time + ".png", dpi=600)
     plt.show()
